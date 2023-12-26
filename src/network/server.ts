da92e0b92d509c4ba69f4976f4982cf6da2085ca
AnthonyLzq <sluzquinosa@uni.pe>
2023-12-26 10:10:11 -0500

    feat: removing fake pub temporarily

--- src/network/server.ts
+++ src/network/server.ts
@@ -2,7 +2,7 @@ import debug from 'debug'
 
 import { dbConnection, firebaseConnection } from 'database'
 import { MAIN_TOPIC } from 'utils'
-import { getClient, start as startMqtt } from './mqtt'
+import { /* getClient, */ start as startMqtt } from './mqtt'
 import { socketConnection } from './socket'
 
 const namespace = `${MAIN_TOPIC}:Mqtt:Server`
@@ -15,13 +15,13 @@ const start = async () => {
     await dbConnection(serverDebug).connect()
   })
 
-  if (process.env.NODE_ENV !== 'local') {
-    const jobs = await import('../jobs')
+  // if (process.env.NODE_ENV !== 'local') {
+  //   const jobs = await import('../jobs')
 
-    ;(Object.keys(jobs) as (keyof typeof jobs)[]).forEach(job => {
-      jobs[job](getClient(serverDebug))
-    })
-  }
+  //   ;(Object.keys(jobs) as (keyof typeof jobs)[]).forEach(job => {
+  //     jobs[job](getClient(serverDebug))
+  //   })
+  // }
 }
 
 export { start }
